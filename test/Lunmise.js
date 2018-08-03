996d3de4b0878f0629edf7375624599ac13f09f9
alun <314884680@qq.com>
2018-08-03 11:16:50 +0800

    finish Lunmise

--- test/Lunmise.js
+++ test/Lunmise.js
@@ -12,20 +12,20 @@ class Lunmise {
     this[error] = null;
     this[resolve_back] = [];
     this[reject_back] = [];
-    real(this.resolve.bind(this), this.reject.bind(this));
+    this.real = real;
+    this.real(this.resolve.bind(this), this.reject.bind(this));
   }
   resolve(res) {
-    // console.log(res)
     if (this[fail] === false) {
       this[success] = true;
       this[result] = res;
-      // console.log(res)
       while (this[resolve_back].length > 0) {
         let next_fn = this[resolve_back].shift();
         let next_result = next_fn(this[result]);
-        console.log(next_result);
         if (next_result instanceof Lunmise) {
+          next_result[success] = false;
           [next_result[resolve_back], next_result[reject_back]] = [this[resolve_back], this[reject_back]]; 
+          next_result.real(next_result.resolve.bind(next_result), next_result.reject.bind(next_result))
           return;
         }
         this[result] = next_result;
